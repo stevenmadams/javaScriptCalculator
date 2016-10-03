09d8f6ea16bc41819ffdabd6674e0bc253a2a505
stevenmadams <stevenadams@me.com>
2016-10-02 21:39:24 -0600

    HEAVILY commented the code/Fixed error where you push equals and then try to continue to math that current equaled number .

--- calc.js
+++ calc.js
@@ -1,15 +1,12 @@
 var alertWindowLoad = function(e) {
-console.log('Window loaded');
-
+console.log('Yer window is loaded');
 //GET CALC DIV
-// var calcdiv = document.getElementById('calc');
 // CREATE TABLE (calctab) INSIDE OF DIV
 var calctable = document.createElement('table');
     calctable.className = 'calc';
 var numbers = document.getElementsByClassName('number');
-// calcdiv.append(calctable);
 var inputNums = [];
-var operands = [];
+var operands = [];  //not used until the doTheMath function
 var currentNumber = '';
 ////////////////////////////////////////////////
 var trow1 = document.createElement('tr');
@@ -23,8 +20,9 @@ var tdClear = document.createElement('td');
     tdClear.className = 'clear';
     tdClear.textContent = 'C';
     tdClear.addEventListener('click', function(e){
-      currentNumber = '';
-      inputNums = [];
+      currentNumber = ''; //resets display to blank
+      inputNums = []; // clears the array of numbers
+      operands = []; //clears the array of operators
     tdTotal.textContent = currentNumber;
     });
     //---------------------------------------------
@@ -57,14 +55,14 @@ var td9 = document.createElement('td');
       currentNumber += '9';
     tdTotal.textContent = currentNumber;
     });
-    //---------------------------------------------
+//DIVIDE---------------------------------------------
 var tdDIV = document.createElement('td');
     tdDIV.className = 'math';
     tdDIV.textContent ='/';
     tdDIV.addEventListener('click', function(e){
-      doTheMath('/');
+      doTheMath('/');  //pushes / to the doTheMath function
     });
-    //---------------------------------------------
+//---------------------------------------------
 calctable.appendChild(trow2);
 calctable.appendChild(td7);
 calctable.appendChild(td8);
@@ -96,12 +94,12 @@ var td6 = document.createElement('td');
       currentNumber += '6';
     tdTotal.textContent = currentNumber;
     });
-    //---------------------------------------------
+// SUBTRACT ---------------------------------------------
 var tdSUB = document.createElement('td');
     tdSUB.className = 'math';
     tdSUB.textContent ='-';
     tdSUB.addEventListener('click', function(e){
-      doTheMath('-');
+      doTheMath('-');  //pushes - to the doTheMath function
     });
     //---------------------------------------------
 calctable.appendChild(trow3);
@@ -135,12 +133,12 @@ var td3 = document.createElement('td');
       currentNumber += '3';
     tdTotal.textContent = currentNumber;
     });
-    //---------------------------------------------
+// MULTIPLY---------------------------------------------
 var tdMULT = document.createElement('td');
     tdMULT.className = 'math';
     tdMULT.textContent ='x'
     tdMULT.addEventListener('click', function(e){
-      doTheMath('*');
+      doTheMath('*');  //pushes * to the doTheMath function
     });
     //---------------------------------------------
 calctable.appendChild(trow4);
@@ -150,17 +148,17 @@ calctable.appendChild(td3);
 calctable.appendChild(tdMULT);
 ////////////////////////////////////////////////
 var trow5 = document.createElement('tr');
-//---------------------------------------------
+//DECIMAL ---------------------------------------------
 var tdDEC = document.createElement('td');
     tdDEC.className = 'number';
     tdDEC.textContent ='.'
     tdDEC.addEventListener('click', function(e){
-      if (!currentNumber.includes('.')){
+      if (!currentNumber.includes('.')){ //if statement so there can only be ONE decimal
         currentNumber += '.';
       }
     tdTotal.textContent = currentNumber;
     });
-    //---------------------------------------------
+// ZERO 0 ---------------------------------------------
 var td0 = document.createElement('td');
     td0.className = 'number';
     td0.textContent ='0'
@@ -168,61 +166,54 @@ var td0 = document.createElement('td');
       currentNumber += '0';
     tdTotal.textContent = currentNumber;
     });
-    //---------------------------------------------
+// EQUALS = ---------------------------------------------
 var tdEQUALS = document.createElement('td');
     tdEQUALS.className = 'equals';
     tdEQUALS.textContent ='='
     tdEQUALS.addEventListener('click', function(e){
-      inputNums.push(tdTotal.textContent);
-      doTheMath(operands[operands.length-1]);
+      doTheMath(operands[operands.length-1]); // PASSES THE LAST OPERAND PUSHED
+      currentNumber = inputNums[0]; //sets the current view to the total recieved by the doTheMath function
+      inputNums = []; // resets the inputNums
+      operands = []; // resets the operands
+    tdTotal.textContent = currentNumber; // sets the view as the total
+
     });
-    //---------------------------------------------
+// ADD---------------------------------------------
 var tdADD = document.createElement('td');
     tdADD.className = 'math';
     tdADD.textContent ='+'
     tdADD.addEventListener('click', function(e){
-      // operands.push('+');
-      doTheMath('+');
+      doTheMath('+'); //pushes + to the doTheMath function
     });
-
     //---------------------------------------------
-//~*~*~*~*~*~*~*~*~*~* DO THE MATH
-var doTheMath = function(passedOp){
+//~* DO THE MATH ~*~*~*~*~*~*~*~*~*~* ~*~*~*~*~*~*~*~*~*~* ~*~*~*~*~*~*~*~*~*~*
+var doTheMath = function(passedOp){ // each symbol button passes an operator to the function
   console.log("OPS " +operands);
-  if(inputNums.length < 1){
-    inputNums.push(currentNumber);
-    operands.push(passedOp);
-    console.log("IN IF " + inputNums);
+  if(operands.length == 0){  // if no operands have been set
+    inputNums.push(currentNumber); // push the current display
+    operands.push(passedOp); //push the current operator
     currentNumber = '';
   }
-  else {
-  if (operands[operands.length-1]!== passedOp) {
-    inputNums.push(currentNumber);
-    console.log("INPUT NUMS IN IF 3 "+inputNums);
-    console.log("IN IF 3 -  PASSED OP " + passedOp + " OP ARRAY " + operands[operands.length-1]);
-    var oper2 = operands[operands.length-1]
-    var tempResult = parseFloat(inputNums[0]) + oper2 + parseFloat(inputNums[1]);
-    inputNums = [];
-    tempResult = eval(tempResult);
-    inputNums.push(tempResult);
-    console.log(tempResult);
-    currentNumber = '';
-    operands.push(passedOp);
+  else { // else if there are operands...
+  if (operands[operands.length-1]!== passedOp && inputNums.length >= 2) { // if the last operator in the array does not match the passed operator
+    var tempResult = parseFloat(inputNums[0]) + operands[operands.length-1] + parseFloat(inputNums[1]); //concat the number and symbol and number
+    inputNums = []; // reset the nums array
+    tempResult = eval(tempResult); // eval the concatted equation
+    inputNums.push(tempResult); // push the total as inputNums[0]
+    currentNumber = ''; //reset the display
+    operands.push(passedOp); // push the passed operator
   }
   else {
-    inputNums.push(currentNumber);
-    console.log("IN IF 2 " + inputNums + "PASSED OP " + passedOp + " OP ARRAY " + operands[operands.length-1]);
-    var oper = passedOp;
-    var tempResult = parseFloat(inputNums[0]) + oper + parseFloat(inputNums[1]);
-    inputNums = [];
-    tempResult = eval(tempResult);
-    inputNums.push(tempResult);
-    console.log(tempResult);
-    currentNumber = '';
-    operands.push(passedOp);
+    inputNums.push(currentNumber); // push the current displayed number to the array
+    var tempResult = parseFloat(inputNums[0]) + passedOp + parseFloat(inputNums[1]); //concat the array
+    inputNums = []; //reset the array
+    tempResult = eval(tempResult); // eval the concatted equation
+    inputNums.push(tempResult); //push the total as inputNums[0]
+    currentNumber = ''; //reset the display
+    operands.push(passedOp); // push the passed operator
   }
 }
-  tdTotal.textContent = inputNums[0];
+  tdTotal.textContent = inputNums[0]; // set the current display to the total of the equation
 };
 //#####################################################
 calctable.appendChild(trow5);
